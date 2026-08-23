97e76bc152545ee56230ce9199a35f61dbd0c021
clevartist <isomdez@gmail.com>
2026-08-23 21:16:20 +0300

    mycommit

--- app/page.tsx
+++ app/page.tsx
@@ -20,6 +20,9 @@ export default function Home() {
             </code>{" "}
             file.
           </h1>
+          <h2>
+            here i am
+          </h2>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
             <a
