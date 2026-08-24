bb45197477c209bf615e82801a85f1360f16dfbc
clevartist <isomdez@gmail.com>
2026-08-25 00:04:35 +0300

    l

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,17 +3,21 @@ export default function Header() {
     <header className="fixed top-0 left-0 w-full z-100 bg-transparent">
       <div className="flex flex-row items-center justify-center container mx-auto px-4 py-4 gap-8 relative">
         {/* Left group */}
-        <div className="flex text-white gap-8">
+        <div className="hidden lg:flex text-white gap-8">
           <button className="cursor-pointer">Фотография</button>
           <button className="cursor-pointer">Видеография</button>
           <button className="cursor-pointer">Архитектура</button>
         </div>
 
         {/* Center image */}
-        <img className="w-30 object-contain" src="/logo.png" />
+        <img className="hidden md:block w-30 object-contain" src="/logo.png" />
+        <img
+          className="absolute left-0 md:hidden w-30 object-contain mt-10 ml-8"
+          src="/logo.png"
+        />
 
         {/* Right group */}
-        <div className="flex text-white gap-8">
+        <div className="hidden lg:flex text-white gap-8">
           <button className="cursor-pointer">Граф. дизайн</button>
           <button className="cursor-pointer">Программирование</button>
           <button className="cursor-pointer">Контакты</button>
@@ -21,7 +25,7 @@ export default function Header() {
 
         {/* Menu dropdown */}
         <input
-          className="text-white absolute right-0 w-8 object-contain mr-6"
+          className="text-white absolute right-0 w-8 object-contain mr-6 mt-10 md:mt-0"
           type="image"
           src="/icons/hamburger_menu.svg"
         />
--- app/page.tsx
+++ app/page.tsx
@@ -7,22 +7,10 @@ export default function Home() {
       {/* Add pt-16 or pt-20 to account for header height */}
       <div className="bg-[url('/home_bg.png')] bg-cover bg-center bg-no-repeat min-h-screen flex justify-end pt-20">
         <div className="w-1/2 flex items-center justify-center">
-          <img className="w-2/3 object-contain" src="/welcome_text.png" />
-        </div>
-      </div>
-      <div className="bg-[url('/home_bg.png')] bg-cover bg-center bg-no-repeat min-h-screen flex justify-end pt-20">
-        <div className="w-1/2 flex items-center justify-center">
-          <img className="w-2/3 object-contain" src="/welcome_text.png" />
-        </div>
-      </div>
-      <div className="bg-[url('/home_bg.png')] bg-cover bg-center bg-no-repeat min-h-screen flex justify-end pt-20">
-        <div className="w-1/2 flex items-center justify-center">
-          <img className="w-2/3 object-contain" src="/welcome_text.png" />
-        </div>
-      </div>
-      <div className="bg-[url('/home_bg.png')] bg-cover bg-center bg-no-repeat min-h-screen flex justify-end pt-20">
-        <div className="w-1/2 flex items-center justify-center">
-          <img className="w-2/3 object-contain" src="/welcome_text.png" />
+          <img
+            className="hidden md:block w-2/3 object-contain"
+            src="/welcome_text.png"
+          />
         </div>
       </div>
     </div>
