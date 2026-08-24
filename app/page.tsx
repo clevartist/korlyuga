86f5618c04ec7d5277d98797b68571fd7d046895
clevartist <isomdez@gmail.com>
2026-08-24 20:46:02 +0300

    a

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="bg-[url('/main_bg.jpg')] bg-cover bg-center bg-no-repeat min-h-screen">
-      <h1>Privet!</h1>
+    <div className="bg-[url('/home_bg.png')] bg-cover bg-center bg-no-repeat min-h-screen">
+      <img className="object-cover" src="/welcome_text.png" />
     </div>
   );
 }
