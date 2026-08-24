f99ce8153bae2498f41a772f1e28f1f90e450e48
clevartist <isomdez@gmail.com>
2026-08-24 20:53:39 +0300

    a

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <div className="bg-[url('/home_bg.png')] bg-cover bg-center bg-no-repeat min-h-screen">
-      <img className="object-cover" src="/welcome_text.png" />
+    <div className="bg-[url('/home_bg.png')] bg-cover bg-center bg-no-repeat min-h-screen flex justify-end">
+      <div className="w-1/2 flex items-center justify-center">
+        <img className="w-2/3 object-contain" src="/welcome_text.png" />
+      </div>
     </div>
   );
 }
