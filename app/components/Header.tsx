57e636afc42cb767c851708051cac945eb67a9c2
clevartist <isomdez@gmail.com>
2026-08-24 22:34:19 +0300

    a

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,9 @@
 export default function Header() {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <header className="fixed top-0 left-0 w-full z-100 bg-transparent">
+      <div className="container mx-auto px-4 py-4 outline-2 outline-solid outline-blue-500">
+        <img src="/logo.png" />
+      </div>
+    </header>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,8 @@ export default function Home() {
   return (
     <div>
       <Header />
-      <div className="bg-[url('/home_bg.png')] bg-cover bg-center bg-no-repeat min-h-screen flex justify-end">
+      {/* Add pt-16 or pt-20 to account for header height */}
+      <div className="bg-[url('/home_bg.png')] bg-cover bg-center bg-no-repeat min-h-screen flex justify-end pt-20">
         <div className="w-1/2 flex items-center justify-center">
           <img className="w-2/3 object-contain" src="/welcome_text.png" />
         </div>
