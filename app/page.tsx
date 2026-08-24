23b8632a04e3da15373510fb828513a9194e6ebb
clevartist <isomdez@gmail.com>
2026-08-24 20:55:55 +0300

    a

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+import Header from "./components/Header";
+
 export default function Home() {
   return (
-    <div className="bg-[url('/home_bg.png')] bg-cover bg-center bg-no-repeat min-h-screen flex justify-end">
-      <div className="w-1/2 flex items-center justify-center">
-        <img className="w-2/3 object-contain" src="/welcome_text.png" />
+    <div>
+      <Header />
+      <div className="bg-[url('/home_bg.png')] bg-cover bg-center bg-no-repeat min-h-screen flex justify-end">
+        <div className="w-1/2 flex items-center justify-center">
+          <img className="w-2/3 object-contain" src="/welcome_text.png" />
+        </div>
       </div>
     </div>
   );
