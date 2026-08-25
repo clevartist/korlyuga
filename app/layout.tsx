3cc585a4c2d0e05432c0ee130ff2c7396e2be4f7
clevartist <isomdez@gmail.com>
2026-08-25 11:30:07 +0300

    j

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import SmoothScroll from "./components/SmoothScroll";
+import Header from "./components/Header";
 
 export const metadata: Metadata = {
   title: "Портфолио",
@@ -11,6 +12,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html lang="ru" className="scroll-smooth md:scroll-auto">
       <body>
+        <Header />
         <SmoothScroll>{children}</SmoothScroll>
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,11 @@
-import Header from "./components/Header";
-
 export default function Home() {
   return (
-    <div>
-      <Header />
-      {/* Add pt-16 or pt-20 to account for header height */}
-      <div className="bg-[url('/home_bg.png')] bg-cover bg-center bg-no-repeat min-h-screen flex justify-end pt-20">
-        <div className="w-1/2 flex items-center justify-center">
-          <img
-            className="hidden md:block w-2/3 object-contain"
-            src="/welcome_text.png"
-          />
-        </div>
+    <div className="bg-[url('/home_bg.png')] bg-cover bg-center bg-no-repeat min-h-screen flex justify-end pt-20">
+      <div className="w-1/2 flex items-center justify-center">
+        <img
+          className="hidden md:block w-2/3 object-contain"
+          src="/welcome_text.png"
+        />
       </div>
     </div>
   );
